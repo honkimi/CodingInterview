f5c99f8652953c7fdd6187360d07137d4cf31c73
Kiminari Homma <cevid_cpp@yahoo.co.jp>
2013-02-03 16:39:51 +0900

    Q1

--- src/code/chap1/Q1.java
+++ src/code/chap1/Q1.java
@@ -1,12 +1,40 @@
 package code.chap1;
 
-public class Q1 {
+import java.util.HashMap;
+import java.util.Map;
 
-    public static void main(String args[]) {
-        System.out.println("test");
+/**
+ * ある文字列が、全てユニークである(重複する文字がない)かどうかを判定するアルゴリズムを実装してください。
+ * また、それを実装するのに新たなデータ構造が使えない場合、どのようにすればよいですか？
+ * @author kimi
+ */
+public class Q1 {
+    public static boolean isUnique(String str) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        String ch;
+        for(int i = 0; i < str.length(); i++) {
+            ch = String.valueOf(str.charAt(i));
+            if (map.containsKey(ch)) {
+                return false;
+            }
+            map.put(ch, 1);
+        }
+        return true;
     }
 
-    public boolean returnTrue() {
+    public static boolean isUnique2(String str) {
+        String ch1, ch2;
+
+        for (int i = 0; i < str.length(); i++) {
+            ch1 = String.valueOf(str.charAt(i));
+            for (int j = i + 1; j < str.length(); j++) {
+                ch2 = String.valueOf(str.charAt(j));
+                if (ch1.equals(ch2)) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 }
--- src/test/chap1/Q1Test.java
+++ src/test/chap1/Q1Test.java
@@ -9,9 +9,62 @@ import code.chap1.Q1;
 public class Q1Test {
 
     @Test
-    public void testReturnTrue() {
-        Q1 obj = new Q1();
-        assertTrue(obj.returnTrue());
+    public void test011() {
+        assertTrue(Q1.isUnique("aiueo"));
     }
 
+    @Test
+    public void test012() {
+        assertTrue(Q1.isUnique("kKiuUytagh15"));
+    }
+
+    @Test
+    public void test013() {
+        assertTrue(Q1.isUnique("123456789[@p]\""));
+    }
+
+    @Test
+    public void test911() {
+        assertFalse(Q1.isUnique("aiueokakikukeko"));
+    }
+
+    @Test
+    public void test912() {
+        assertFalse(Q1.isUnique("112235786342"));
+    }
+
+    @Test
+    public void test913() {
+        assertFalse(Q1.isUnique("[@@p:][fjlgawl"));
+    }
+
+    @Test
+    public void test021() {
+        assertTrue(Q1.isUnique2("aiueo"));
+    }
+
+    @Test
+    public void test022() {
+        assertTrue(Q1.isUnique2("kKiuUytagh15"));
+    }
+
+    @Test
+    public void test023() {
+        assertTrue(Q1.isUnique2("123456789[@p]\""));
+    }
+
+    @Test
+    public void test921() {
+        assertFalse(Q1.isUnique2("aiueokakikukeko"));
+    }
+
+    @Test
+    public void test922() {
+        assertFalse(Q1.isUnique2("112235786342"));
+    }
+
+    @Test
+    public void test923() {
+        assertFalse(Q1.isUnique2("[@@p:][fjlgawl"));
+    }
 }
