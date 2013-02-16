747706285abcd99b05d594abf7e1391eca1456fe
Kiminari Homma <cevid_cpp@yahoo.co.jp>
2013-02-16 12:34:32 +0900

    chap2 q1

---
+++ src/test/chap2/Q1Test.java
@@ -0,0 +1,14 @@
+package test.chap2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Q1Test {
+
+    @Test
+    public void test() {
+        fail("まだ実装されていません");
+    }
+
+}
